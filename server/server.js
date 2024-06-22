e9300c897ac78ebdb8ba527324477dea3c553664
Ashakuur1 <engshakrayare114@gmail.com>
2024-06-22 18:02:13 +0300

    chore: Update server.js with error middleware, user routes, and dotenv configuration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
-import express from "express"
+import express from "express";
+import ErrorHandler from "./middlewares/error-middleware.js";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user-routes.js";
 
-const app = express()
+dotenv.config();
+const app = express();
 
-const PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/users", userRoutes);
+app.use(ErrorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
