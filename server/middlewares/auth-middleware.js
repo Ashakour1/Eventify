0ced76ffb6511f360d12d81448c316955f825cdb
Ashakuur1 <engshakrayare114@gmail.com>
2024-06-23 22:20:56 +0300

    Update authMiddleware to use JWT_SECRET from config

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -1,43 +1,39 @@
 import AsyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 import prisma from "../config/db.js";
+import { JWT_SECRET } from "../config/config.js";
 
 export const authMiddleware = AsyncHandler(async (req, res, next) => {
-  let token;
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decoded);
-
-      console.log(process.env.JWT_SECRET);
-      const user = await prisma.user.findUnique({
-        where: {
-          id: decoded._id,
-        },
-      });
-      console.log(user);
-      req.user = user;
-
-      if (!user) {
-        res.status(404);
-        throw new Error("Not Authorized to access this route");
-      }
-
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(401);
-      throw new Error("Not authorized, token failed");
-    }
-  }
+  let token = req.cookies.token;
 
   if (!token) {
-    res.status(401);
-    throw new Error("Not authorized, no token");
+    res.status(403).json({ message: "Not authorized, no token" });
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    // console.log(decoded);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decoded._id,
+      },
+    });
+
+    // console.log(user.id);
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ message: "Not authorized, token failed" });
   }
 });
