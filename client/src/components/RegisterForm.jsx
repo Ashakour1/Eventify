0a38dccec971f05be905cc703d18f81e54055ca9
Ashakuur1 <engshakrayare114@gmail.com>
2024-07-07 22:23:52 +0300

    Update RegisterForm component to handle form submission and display loading state

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -22,14 +22,6 @@ const RegisterForm = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const clearForm = () => {
-    setFormData({
-      username: "",
-      email: "",
-      password: "",
-    });
-  };
-
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -38,11 +30,18 @@ const RegisterForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const { data } = await axios.post("api/users/auth/signup", formData);
       toast.success(data.message);
-      clearForm();
+      setLoading(false);
+      setFormData({
+        username: "",
+        email: "",
+        password: "",
+      });
     } catch (error) {
+      setLoading(false);
       toast.error(error.response.data.message);
       console.log(error);
     }
@@ -52,7 +51,7 @@ const RegisterForm = () => {
     <div className="w-full">
       <Card className="">
         <CardHeader>
-          <CardTitle className="text-blue-500">Register Form </CardTitle>
+          <CardTitle className="text-customBlue">Register Form </CardTitle>
           <CardDescription>
             Fill Out the form carefully for registration
           </CardDescription>
@@ -63,7 +62,10 @@ const RegisterForm = () => {
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="username">Username</Label>
                 <Input
+                  type="text"
+                  name="username"
                   onChange={handleChange}
+                  value={formData.username}
                   id="username"
                   placeholder="Enter your Username"
                 />
@@ -71,7 +73,10 @@ const RegisterForm = () => {
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="email">Email</Label>
                 <Input
+                  type="email"
+                  name="email"
                   onChange={handleChange}
+                  value={formData.email}
                   id="email"
                   placeholder="Enter your Email"
                 />
@@ -79,15 +84,33 @@ const RegisterForm = () => {
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="password">Password</Label>
                 <Input
+                  name="password"
+                  type="password"
                   id="password"
+                  value={formData.password}
                   className="focus-none"
                   placeholder="Enter your password"
                   onChange={handleChange}
                 />
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Button className="bg-blue-500 hover:bg-none" type="submit">
-                  Register
+                <Button className="bg-blue-500 hover:bg-blue-0" type="submit">
+                  {loading ? (
+                    <svg
+                      class="animate-spin border-white border-2 rounded-full h-5 w-5 mr-3"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        class="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    "Register"
+                  )}
                 </Button>
               </div>
             </div>
@@ -95,7 +118,7 @@ const RegisterForm = () => {
         </CardContent>
         <CardFooter className="justify-center flex items-center text-center">
           Have an account?&nbsp;
-          <Link to="/login" className="font-bold text-blue-500">
+          <Link to="/login" className="font-bold text-customBlue">
             Login
           </Link>
         </CardFooter>
