db85f6e5a8a3e0ef1243d94f29d1eb3b4d781f6b
Ashakuur1 <engshakrayare114@gmail.com>
2024-07-08 17:00:55 +0300

    Update routing in App component and add NotFound component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,24 @@ import HeroSection from "./pages/Hero-section";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import { Toaster } from "react-hot-toast";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <>
       <Toaster />
       <Router>
-        <Header />
         <Routes>
-          <Route path="/" element={
-            <>
-              <HeroSection />
-          </>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <HeroSection />
+              </>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </Router>
