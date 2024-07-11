5ae1e6d224b63e164d17c31e781f451d1c63ea8c
Ashakuur1 <engshakrayare114@gmail.com>
2024-07-11 23:12:27 +0300

    Update HeroSection component with improved messaging and button styling

--- client/src/pages/Hero-section.jsx
+++ client/src/pages/Hero-section.jsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import { Link } from "react-router-dom";
 import { TypewriterEffect } from "@/components/ui/typewriter-effect";
+import { FaArrowDown } from "react-icons/fa";
 
 const HeroSection = () => {
   const words = [
@@ -37,11 +38,23 @@ const HeroSection = () => {
     },
   ];
 
- 
   return (
     <main className="">
-      <div className="max-w-[1040px] mx-auto min-h-screen">
-        <div className="flex flex-col justify-center items-center text-center w-9/12 mx-auto pt-24 space-y-6">
+      <div className="max-w-[1040px] mx-auto h-[540px]">
+        <div className="flex flex-col justify-center items-center text-center w-9/12 mx-auto pt-10 space-y-6">
+          <div className="inset-0">
+            <Link
+              className="px-3  text-sm font-medium inline-flex items-center justify-center border rounded-full  text-zinc-600 hover:text-black transition duration-150 ease-in-out w-full group border-black"
+              to="/contact"
+            >
+              <span className="relative inline-flex items-center">
+                Contact Us
+                <span className="tracking-normal text-primary-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
+                  -&gt;
+                </span>
+              </span>
+            </Link>
+          </div>
           <TypewriterEffect words={words} fontSize="text-3xl" />
 
           <p className="text-lg text-gray-500 ">
@@ -54,16 +67,22 @@ const HeroSection = () => {
             <Link to="/register">
               <button
                 type="button"
-                className="border-customBlue rounded-md border-2 bg-customBlue hover:bg-customBlue/90 px-6 py-2 text-white font-semibold"
+                className="rounded-md  bg-customBlue hover:bg-customBlue/90 px-6 py-2 text-white text-sm font-medium"
               >
                 Get Started
               </button>
             </Link>
             <Link to="/">
-              <button className="border-customBlue rounded-md border-2 bg-transparent hover:bg-none px-6 py-2 text-black font-semibold">
+              <button className="border-customBlue rounded-md border shadow-sm transition-colors bg-white hover:bg-none px-6 py-2 text-black text-sm font-medium">
                 Learn More
               </button>
             </Link>
+          </div>
+          <div class="  items-center pt-32  ">
+            <FaArrowDown
+              icon="mdi:arrow-down"
+              className="text-3xl text-blue-500 animate-bounce w-6 h-6"
+            ></FaArrowDown>
           </div>
         </div>
       </div>
