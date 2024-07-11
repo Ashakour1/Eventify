cab241b91bb2badb8dac1e5ef49c3684a5c1029b
Ashakuur1 <engshakrayare114@gmail.com>
2024-07-11 23:15:16 +0300

    Update Header component with improved styling and link to AboutPage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,11 +3,11 @@ import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <header className="border-b-2 mx-20 my-2 rounded px-4 py-2">
+    <header className="mx-20 my-2 rounded px-4 py-2">
       <div className="max-w-[1200px] mx-auto flex justify-between items-center ">
         <div className="logo">
           <Link to="/">
-            <p className="text-2xl font-bold">Eventify</p>
+            <p className="text-2xl font-bold text-blue-500">Eventify</p>
           </Link>
         </div>
         <div className="hidden md:flex items-center space-x-3">
@@ -20,8 +20,8 @@ const Header = () => {
           <Link className="text-sm p-2 font-medium " to="/contact">
             Contact Us
           </Link>
-          <Button className="rounded-none">
-            <Link to="/reg">Get Started</Link>
+          <Button className="rounded-none bg-blue-500">
+            <Link to="/reg" >Get Started</Link>
           </Button>
         </div>
       </div>
