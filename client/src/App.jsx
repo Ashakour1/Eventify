1f2e253a0a3fccbb5b0bc40caef2d177323f9bb3
Zakarie <116062502+zakariem@users.noreply.github.com>
2024-07-11 12:08:35 +0300

    Add About and Contact pages and update Header link
    Added AboutPage and ContactPage components to the App and updated the Header component to link to the AboutPage instead of the home page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Header from "./components/Header";
 import HeroSection from "./pages/Hero-section";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContectPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import { Toaster } from "react-hot-toast";
@@ -11,18 +13,20 @@ function App() {
     <>
       <Toaster />
       <Router>
+        <Header />
         <Routes>
           <Route
             path="/"
             element={
               <>
-                <Header />
                 <HeroSection />
               </>
             }
           />
           <Route path="*" element={<NotFound />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
     </>
