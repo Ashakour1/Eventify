16a7b0053f90678a0b0d95a61b5fd6a75c3d64cb
Ashakuur1 <engshakrayare114@gmail.com>
2024-07-11 23:15:39 +0300

    Refactor RegisterPage import to use kebab-case file name convention

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,10 @@ import HeroSection from "./pages/Hero-section";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContectPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
+import RegisterPage from "./pages/Register-page";
 import { Toaster } from "react-hot-toast";
 import NotFound from "./components/NotFound";
+import LoginPage from "./pages/Login-page";
 
 function App() {
   return (
@@ -27,6 +28,8 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<LoginPage/>}
+          />
         </Routes>
       </Router>
     </>
