300bf657a3bed5f04e8a4048d3af96931e358fac
Zakarie <116062502+zakariem@users.noreply.github.com>
2024-07-12 10:30:36 +0300

    Add email sending

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import ErrorHandler from "./middlewares/error-middleware.js";
 import userRoutes from "./routes/user-routes.js";
 import eventRoutes from "./routes/event-routes.js";
+import { sendEmail } from "./controllers/email-controller.js";
 import { PORT } from "./config/config.js";
 import cookieParser from "cookie-parser";
 
@@ -12,8 +13,11 @@ const Port = PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/users", userRoutes);
 app.use("/api/events", eventRoutes);
+app.use("/api/contact", sendEmail);
+
 app.use(ErrorHandler);
 
 app.listen(Port, () => {
