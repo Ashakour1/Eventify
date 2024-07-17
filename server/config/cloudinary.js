cda3359c776b1127c9175f485b56c157e6f4f31d
Ashakuur1 <engshakrayare114@gmail.com>
2024-07-17 04:32:42 +0300

    chore: Update Cloudinary API credentials
    This commit updates the Cloudinary API credentials in the `cloudinary.js` configuration file. The `api_key` and `api_secret` values have been changed to "847887538698144" and "HEaLve4ACVsUtEmUnsWNqlg68r0" respectively.

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -3,9 +3,9 @@ import {CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET} from "./
 
 // configuration
 cloudinary.v2.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: "847887538698144",
+    api_secret: "HEaLve4ACVsUtEmUnsWNqlg68r0",
 });
 
 export default cloudinary.v2;
