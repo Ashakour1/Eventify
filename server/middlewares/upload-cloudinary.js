c51c2236b0f6cf9960b8282cd4f6278b0fa1451c
Ashakuur1 <engshakrayare114@gmail.com>
2024-07-17 04:32:31 +0300

    chore: Update multer storage to use memory storage and set file size limit

--- server/middlewares/upload-cloudinary.js
+++ server/middlewares/upload-cloudinary.js
@@ -1,11 +1,12 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
   },
 });
-
-const upload = multer({ storage: storage });
 
 export default upload;
